6df43070bc45505aaed6688e51ec3802381841c9
Deive Oliveira <deiveoliveira1@gmail.com>
2024-01-18 12:11:33 -0300

    Ajustando cabeçalho

--- menu.js
+++ menu.js
@@ -1,6 +1,7 @@
 let btnMenu = document.getElementById('btn-abrir-menu')
 let menu = document.getElementById('menu-mobile')
 let overlay = document.getElementById('overlay-menu')
+let cont = document.getElementById('botao-contato')
 
 btnMenu.addEventListener('click', ()=>{
     menu.classList.add('abrir-menu')
@@ -13,3 +14,9 @@ menu.addEventListener('click', ()=>{
 overlay.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
 })
+
+cont.addEventListener('click',() =>{
+    let section = document.getElementById('contato');
+    section.scrollIntoView({behavior: "smooth"});
+}) 
+    
